6c8215ee4f31f75bb3aa41b140a64ab09605c5c7
haruki-N <nagasawa_haruki@cyberagent.co.jp>
2025-09-01 09:43:15 +0900

    add: main logic

--- src/main.py
+++ src/main.py
@@ -1,11 +1,33 @@
 import os
-
+from config.prompts import triage_system_prompt, triage_user_prompt, prompt_instructions
+from config.var import profile, email
+from models import llm_router
 from dotenv import load_dotenv
 load_dotenv()
 
 
 def main():
-    print("Hello from email-agent!")
+    system_prompt = triage_system_prompt.format(
+        full_name=profile["full_name"],
+        name=profile["name"],
+        examples=None,
+        user_profile_background=profile["user_profile_background"],
+        triage_no=prompt_instructions["triage_rules"]["ignore"],
+        triage_notify=prompt_instructions["triage_rules"]["notify"],
+        triage_email=prompt_instructions["triage_rules"]["respond"],
+    )
+    user_prompt = triage_user_prompt.format(
+        author=email["from"],
+        to=email["to"],
+        subject=email["subject"],
+        email_thread=email["body"],
+    )
+    response = llm_router.invoke(
+        [
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": user_prompt},
+        ]
+    )
 
 
 if __name__ == "__main__":
